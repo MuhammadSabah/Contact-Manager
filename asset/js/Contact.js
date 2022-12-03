bb32c77c0121f1dbabab8cc4d81ed5a1f1f3b84a
Muhammad Ibrahim <hama.s.ibrahim9@gmail.com>
2022-12-03 14:44:44 +0300

    first commit

---
+++ asset/js/Contact.js
@@ -0,0 +1,10 @@
+class Contact {
+  constructor(id, name, phone, email) {
+    this.id = id;
+    this.cname = name;
+    this.phone = phone;
+    this.email = email;
+  }
+}
+
+export default Contact;
